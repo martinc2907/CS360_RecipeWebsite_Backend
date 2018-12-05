b13428918a3ef0cbe70e221542cdb93af941d567
Changyoon <cyoon47@hotmail.com>
2018-12-05 16:42:39 +0900

    write_recipe fixed

--- app.js
+++ app.js
@@ -7,10 +7,19 @@ const db = mysql.createConnection({
 	host : 'jihye-mysql.cmlpaiveb1r5.us-east-2.rds.amazonaws.com',
 	user : 'jihye',
 	password : 'makefood',
-	database: 'recipe'
+	database: 'recipe',
 });
 
-//connect to mysql
+// const db = mysql.createPool({
+// 	host : 'jihye-mysql.cmlpaiveb1r5.us-east-2.rds.amazonaws.com',
+// 	port: 3306,
+// 	user : 'jihye',
+// 	password : 'makefood',
+// 	database: 'recipe',
+// 	connectionLimit: 20,
+// });
+
+// connect to mysql
 db.connect((err) => {
 	if(err){
 		throw err;
@@ -55,6 +64,25 @@ app.post('/create_user', (req,res) =>{
 	var Username = db.escape(req.body.Username);	//escape to keep the quote.
 	var Password = db.escape(req.body.Password);
 	var sql = `INSERT INTO USER (Username, Password) VALUES (${Username}, ${Password})`;
+
+	// db.getConnection(function(err,connection){
+	// 	var query = connection.query({
+	// 		sql: sql,
+	// 		timeout: 10000,
+	// 	}, function(err, rows, fields) {
+	// 		if(err) {
+	// 			console.log(err);
+	// 			res.send({success: false});
+	// 		}
+	// 		else {
+	// 			res.send({successful: true});
+	// 		}
+	// 		connection.release()
+	// 	});
+	// });
+
+	
+
 	db.query(sql, (err,result)=>{
 		if(err){
 			console.log(err);
@@ -68,6 +96,7 @@ app.post('/create_user', (req,res) =>{
 
 //Write Recipe
 app.post('/write_recipe', (req,res)=>{
+	// TODO: FIX ERROR WHERE INGREDIENTS & COST DO NOT GET UPDATED IF TITLE LONGER THAN 30 CHARACTERS
 	console.log(req.body);
 
 	var Title = db.escape(req.body.Title);
@@ -96,7 +125,9 @@ app.post('/write_recipe', (req,res)=>{
 		}
 	});
 
-	//must add recipe first(with total cost 0)
+	console.log("CHECK FOR ERROR BUT NO RETURN")
+
+	//must add recipe first	(with total cost 0)
 	var sql_insert2 = `INSERT INTO RECIPE 
 						VALUES (${Title}, ${Instruction},${Time},${Difficulty},${Total_cost},${Picture_url},${Rating},${User})`;
 	db.query(sql_insert2, (err,result)=>{
@@ -107,58 +138,59 @@ app.post('/write_recipe', (req,res)=>{
 			res.send({success:false});
 			return;
 		}
-	});
+		else {
+			//add to USES
+			var length = Ingredients.length;
+			for(var i = 0; i < length; i++){
+				var sql_insert = `INSERT INTO USES VALUES (${db.escape(Ingredients[i])}, ${Title},${Ingredients_quantity[i]})`;
+				db.query(sql_insert, (err,result)=>{
+					if(err){
+						console.log(err);
+						//SHOULD ROLL BACK. do start/commit/end transaction?
+						db.query(sql_rollback, (err,result)=>{});
+						res.send({success:false});
+						return;
+					}
+					else {
+						//calculate total cost
+						var sql_sum = `SELECT SUM(I.Cost*U.Ingredient_quantity) AS SUM
+										FROM INGREDIENT AS I,USES AS U 
+										WHERE U.RECI_Title = ${Title} AND U.INGR_Name = I.Name`;
+						db.query(sql_sum, (err,result)=>{
+							if(err){
+								console.log(err);
+								//SHOULD ROLL BACK. do start/commit/end transaction?
+								db.query(sql_rollback, (err,result)=>{});
+								res.send({success:false});
+								return;
+							}
+							console.log(result);
+							console.log(result[0].SUM);
+							Total_cost = result[0].SUM;
+							console.log(Total_cost);
 
 
-	//add to USES
-	var length = Ingredients.length;
-	for(var i = 0; i < length; i++){
-		var sql_insert = `INSERT INTO USES VALUES (${db.escape(Ingredients[i])}, ${Title},${Ingredients_quantity[i]})`;
-		db.query(sql_insert, (err,result)=>{
-			if(err){
-				console.log(err);
-				//SHOULD ROLL BACK. do start/commit/end transaction?
-				db.query(sql_rollback, (err,result)=>{});
-				res.send({success:false});
-				return;
+							//Update recipe for total cost- do inside of callback for no async problem.
+							var sql_update = `UPDATE RECIPE
+									SET Total_cost = ${Total_cost}
+									WHERE Title = ${Title}`;
+							db.query(sql_update, (err,result)=>{
+								if(err){
+									console.log(err);
+									db.query(sql_rollback, (err,result)=>{});
+									res.send({success:false});
+									return;
+								}
+								//everything success
+								console.log("Recipe written");
+								db.query(sql_commit, (err,result)=>{});
+								res.send({success:true});
+							});
+						});
+					}
+				});
 			}
-		});
-	}
-
-	//calculate total cost
-	var sql_sum = `SELECT SUM(I.Cost*U.Ingredient_quantity) AS SUM
-					FROM INGREDIENT AS I,USES AS U 
-					WHERE U.RECI_Title = ${Title} AND U.INGR_Name = I.Name`;
-	db.query(sql_sum, (err,result)=>{
-		if(err){
-			console.log(err);
-			//SHOULD ROLL BACK. do start/commit/end transaction?
-			db.query(sql_rollback, (err,result)=>{});
-			res.send({success:false});
-			return;
 		}
-		console.log(result);
-		console.log(result[0].SUM);
-		Total_cost = result[0].SUM;
-		console.log(Total_cost);
-
-
-		//Update recipe for total cost- do inside of callback for no async problem.
-		var sql_update = `UPDATE RECIPE
-						SET Total_cost = ${Total_cost}
-						WHERE Title = ${Title}`;
-		db.query(sql_update, (err,result)=>{
-			if(err){
-				console.log(err);
-				db.query(sql_rollback, (err,result)=>{});
-				res.send({success:false});
-				return;
-			}
-			//everything success
-			console.log("Recipe written");
-			db.query(sql_commit, (err,result)=>{});
-			res.send({success:true});
-		});
 	});
 });
 
